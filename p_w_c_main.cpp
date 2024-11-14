c8dfa7a7e8aae0eff2c533ead1632eb30071b195
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-11-14 15:55:05 -0600

    Update p_w_c_main.cpp

--- p_w_c_main.cpp
+++ p_w_c_main.cpp
@@ -6,6 +6,42 @@ Power wisdon courage
 #include <vector>
 using namespace std;
 
+class TrianguleNumberCalculator 
+{
+public:
+
+int value(int n){
+    
+    if (n == 0 || n <= 0){
+        return 0;
+    }
+    
+    return n;
+}
+
+int add(int n, int m){
+    if (n == 0 || n <= 0 || m == 0 || m <= 0){
+        return 0;
+    }
+    
+    return n;
+}
+
+int subtract (int n, int m){
+    if (n == 0 || n <= 0 || m == 0 || m <= 0){
+        return 0;
+    }
+    else 
+
+
+    
+    return n;
+}
+
+private:
+
+};
+
 int main(){
     
 }
